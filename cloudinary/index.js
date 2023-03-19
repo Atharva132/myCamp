a44b22c75709157fcf3bbba399eb7143141497c3
Atharva132 <atharvaamte@gmail.com>
2023-03-19 16:28:54 +0530

    rename

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'YelpCamp',
+        folder: 'myCamp',
         allowedFormats: ['jpeg', 'png', 'jpg']
     }
 });
